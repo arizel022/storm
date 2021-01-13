9a8991d293842cd4a92a953a535590ffa38ff3df
arizel022 <arizel022@gmail.com>
2021-01-13 21:52:13 +0100

    add active-menu, fixed classes

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,17 @@ $(document).ready(function () {
 
     $('.faq__ans').hide();
     $('.faq__item-wrapper').click(function () {
+        $(this).toggleClass('active');
         $(this).siblings('.faq__ans').slideToggle('slow', function() {
             $(".faq__ans").not(this).hide('slow');
         });
     });
+
+
+    $('.menu-btn').click(function () {
+        $(this).toggleClass('active');
+    });
+    
 
 
 
