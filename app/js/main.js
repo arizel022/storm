179873e7804b2f1883850a12d773fde1069bd49a
arizel022 <arizel022@gmail.com>
2021-01-20 21:12:15 +0100

    add portfolio-items, fixed mixer and magnific

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,45 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+// });
 
-//-----------------------------------------гармошка блока faq
+	//-----------------------------------------гармошка блока faq
     $('.faq__ans').hide();
     $('.faq__item-wrapper').click(function () {
         $(this).toggleClass('active');
         $(this).siblings('.faq__ans').slideToggle('slow', function() {
-            $(".faq__ans").not(this).hide('slow');
+            $('.faq__ans').not(this).hide('slow');
         });
-    });
+	});
+
 //-----------------------------------------работа кнопки меню
     $('.menu-btn').click(function () {
         $(this).toggleClass('active');
-    });
+	});
+
 //-----------------------------------------спрятать меню при сворачивании панели
     $('.header__video__layer').mouseleave(function () {
         $('.menu-btn').removeClass('active');
-    });
+	});
+
+
+//-----------------------------------------спрятать меню при клике вне меню
+	$(document).mouseup(function (e){ // отслеживаем клик
+		var block = $(".menu-btn"); //кнопка, меню В ней
+		if (!block.is(e.target)&& block.has(e.target).length === 0) { //если клик не по кнопке/меню/чайлдам
+			$('.menu-btn').removeClass('active'); //кнопка неактивна
+		}
+	});
+
 //-----------------------------------------backToTopButton
     $(window).scroll(function() {
-        if
-        ($(this).scrollTop() != 0) {
-            $('#toTop').fadeIn();
-        } else {
-            $('#toTop').fadeOut();
-        }
+        if ($(this).scrollTop() != 0) {
+			$('#toTop').fadeIn();} 
+		else {
+            $('#toTop').fadeOut();}
     });
     $('#toTop').click(function() {
         $('body,html').animate({scrollTop:0},800);
-    });
+	});
+	
 //-----------------------------------------E-mail Ajax Send
 	$(".backform__form").submit(function() {
 		var th = $(this);
@@ -42,7 +54,8 @@ $(document).ready(function () {
 			}, 1000);
 		});
 		return false;
-    });
+	});
+	
 //-----------------------------------------вызов попапа заказать
     $('.order__wrapper').hide();
 	$('.why__btn.btn-main--act').click(function() {
@@ -62,11 +75,48 @@ $(document).ready(function () {
 			$(this).fadeOut();					
 		}
 	});
-//-----------------------------------------
-var mixer = mixitup('.works');
 
 
 
+//-----------------------------------------magnific
+$(document).ready(function() {
+	$('.card__gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small></small>'; //мелкий текст между тегами
+			}
+		}
+	});
 });
+
+//-----------------------------------------slick
+	$('.slider--portfolio').slick({
+		dots: false,
+		infinite: true,
+		arrows: false,
+		autoplay: true
+	});
+	//-----------------------------------------mixer
+	// var mixer = mixitup('.works');
+	//проверка для миксера, есть ли его элементы на странице - вызов впустую дает ошибку в консоли 
+	var containerEl = document.querySelector('.works');
+	var mixer;
+	if (containerEl) {
+		mixer = mixitup(containerEl, {
+			selectors: {control: '.works__btn'},
+			load: {filter: '.works__item'}
+		});
+	}
+
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        // 'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/magnific-popup/dist/magnific-popup.css',
         // 'node_modules/rateyo/src/jquery.rateyo.css',
         // 'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
         // magnific-popup - всплывающие окна, rateyo - звезды рейтинга, rangeSlider - регулятор цены == плагины отключены, если надо - включить, новые подключаются по аналогии.
@@ -33,7 +33,7 @@ gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/mixitup/dist/mixitup.js',
-        // 'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         // 'node_modules/rateyo/src/jquery.rateyo.js',
         // 'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     ])
@@ -42,7 +42,7 @@ gulp.task('script', function(){
         .pipe(gulp.dest('app/js'))
 });
 gulp.task('html', function(){
-    return gulp.src('app/*.html')
+    return gulp.src('app/**/*.html')
     .pipe(browserSync.reload({stream: true}))
 });
 gulp.task('js', function(){
@@ -58,7 +58,7 @@ gulp.task('browser-sync', function() {
 });
 gulp.task('watch', function(){
     gulp.watch('app/scss/**/*.scss', gulp.parallel('sass')); // указываем, за кем следим. Если тот, за кем следим, меняется, то выполняется таск sass.
-    gulp.watch('app/*.html', gulp.parallel('html'));   //
+    gulp.watch('app/**/*.html', gulp.parallel('html'));   //
     gulp.watch('app/js/*.js', gulp.parallel('js'));  // чтобы за файлами хтмл и джс было слежение.
 });
 
